fc9d86926ad34d66cfedea28f0a4dd18b07e3751
mancreative <mancreative@hotmail.com>
2022-03-29 12:20:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import { useAddress, useDisconnect, useMetamask, useNFTDrop } from '@thirdweb-dev/react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   const disconnectWallet = useDisconnect();
+  const nftDrop = useNFTDrop('0x82747Bd4e435C9D5cF2342c8361B19910259C264');
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+  const [isClaming, setIsClaming] = useState(false);
+
+  useEffect(() => {
+    if(!address){
+      return;
+    }
+    const checkBalance = async () => {
+      try{
+        const nfts = await nftDrop.getOwned(address);
+        setHasClaimedNFT(nfts?.length > 0);
+
+      }catch(error){
+        setHasClaimedNFT(false);
+        console.error('Failed to mint NFT', error);
+      }
+    }
+  });
+
+  const mintNft = async () => {
+
+    try {
+
+      setIsClaming(true);
+      await nftDrop.claim(1);
+      setHasClaimedNFT(true);
+
+    } catch (error) {
+
+      setHasClaimedNFT(false);
+      console.error('Failed to mint NFT', error);
+
+    } finally {
+
+      setIsClaming(false);
+    }
+
+  };
+
+  if(!address){
+    return (<button onClick={connectWithMetamask}>Connect with Metamask</button>);
+  }
+
+  if(hasClaimedNFT){
+    return (
+      <>
+        🎉 You have a memebership NFT
+      </>
+    )
+  }
+
   return (
-    <div>
-      {address ? (
-        <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
-          <p>Your address: {address}</p>
-        </>
-      ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
-      )}
-    </div>
+    <>
+      <p>Your address: {address}</p>
+      <button disabled={isClaming} onClick={mintNft}>Mint NFT</button>
+      <button onClick={disconnectWallet}>Disconnect Wallet</button>
+    </>
   );
 }
 
